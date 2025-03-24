9599c773916ec88a05ab3e23f30988c18e732380
Kiwoon Jung | Pacific First Aid Ltd <Kiwoon@pacificfirstaid.ca>
2025-03-24 16:19:05 -0700

    20250324 16:18 added vercel.json file

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/auth.js";
 import customerRoutes from "./routes/companyInfo.js";
 import errorHandler from "./middleware/errorHandler.js";
 import logger from "./middleware/logger.js";
-// import auth from "./middleware/auth.js";
 import cors from "cors"; // Corrected import from the cors package
 
 const app = express();
@@ -14,23 +13,27 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*", // Allow requests from any origin (use cautiously)
+    origin: [
+      "chrome-extension://bkcjiigjjabcnlkcnjkbdeacehmcpbdn", // Your specific extension origin
+      "*", // Allow all origins, use carefully in production
+    ],
   })
 );
+
 app.use(logger); // Logging middleware
 app.use(express.json()); // Parse JSON request bodies
 
 // Error handling middleware (must be the last middleware)
 app.use(errorHandler);
 
-/* ROUTES */
+// ROUTES
 app.use("/auth", authRoutes);
 app.use("/invoice", invoiceRoutes);
 app.use("/companyInfo", companyRoutes);
 app.use("/customer", customerRoutes);
 
 // Start the server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Proxy server running on http://localhost:${PORT}`);
 });
