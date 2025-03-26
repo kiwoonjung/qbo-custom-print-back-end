de5065c811e2ad9dc80167a4f0fcf4483b4e1641
Kiwoon Jung | Pacific First Aid Ltd <Kiwoon@pacificfirstaid.ca>
2025-03-26 16:22:51 -0700

    20250326 16:22 added postgres at db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import postgres from "postgres";
 
 dotenv.config();
 
-const { Pool } = pg;
+const { Pool } = postgres;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // Use Supabase URL from env variables
   ssl: {
     rejectUnauthorized: false, // Required for Supabase
   },
-  host: "db.rmcukybxmacfbisyzfoj.supabase.co", // Explicitly set host
-  dnsFamily: 4,
 });
 
 const createTable = async () => {
@@ -37,3 +34,43 @@ const createTable = async () => {
 createTable();
 
 export default pool;
+
+// import pg from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const { Pool } = pg;
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL, // Use Supabase URL from env variables
+//   ssl: {
+//     rejectUnauthorized: false, // Required for Supabase
+//   },
+//   host: "db.rmcukybxmacfbisyzfoj.supabase.co", // Explicitly set host
+//   dnsFamily: 4,
+// });
+
+// const createTable = async () => {
+//   const query = `
+//       CREATE TABLE IF NOT EXISTS tokens (
+//         id SERIAL PRIMARY KEY,
+//         access_token TEXT NOT NULL,
+//         refresh_token TEXT NOT NULL,
+//         realm_id TEXT NOT NULL UNIQUE,
+//         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+//       );
+//     `;
+
+//   try {
+//     await pool.query(query);
+//     console.log("✅ Tokens table is ready in Supabase!");
+//   } catch (error) {
+//     console.error("❌ Error creating tokens table:", error);
+//   }
+// };
+
+// // Run table creation when server starts
+// createTable();
+
+// export default pool;
