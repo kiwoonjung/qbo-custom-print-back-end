bd6a6955b4eba74a04b65a4418f030ff171af6f8
Kiwoon Jung | Pacific First Aid Ltd <Kiwoon@pacificfirstaid.ca>
2025-03-26 13:22:16 -0700

    fixed

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 /* READ */
 router.get("/get-client-id", getClientId); // Endpoint to get client ID
-router.get("/get-access-token", getAccessToken); // New route for getting access token
+router.get("/get-access-token", getAccessToken); // New route for getting access token!
 /* POST */
 router.post("/exchange-code", exchangeCodeForToken); // Endpoint to exchange authorization code for tokens
 
